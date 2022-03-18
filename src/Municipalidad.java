e8a52879246358f27dc7cc0441f4624a1410fb79
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-03-18 14:09:32 -0300

    Se agregó metodo para sumar deudas

--- src/Municipalidad.java
+++ src/Municipalidad.java
@@ -1,10 +1,3 @@
-/*
--Se debe desarrollar un programa en Java que solicite por teclado los datos de los planes y de los pagos y luego de finalizada la carga informe:
-    * Cantidad de planes pagados en su totalidad (es decir, que tengan tantos pagos recibidos como la cantidad de cuotas pactadas)
-    * Sumatoria de las deudas registradas
-    * Listado de todos los pagos efectuados por un contribuyente en particular
-    * Promedio general de intereses adicionales cobrados
- */
 public class Municipalidad {
     Plan [] planes;
     
@@ -34,6 +27,15 @@ public class Municipalidad {
     }
     
     
+    // Sumatoria de las deudas registradas
+    public float getSumatoriaDeudas(){
+        float acumuladorDeudas = 0f;
+        for (Plan p : planes) {
+            acumuladorDeudas += p.getDeudaTotal();
+        }
+        return acumuladorDeudas;
+    }
+   
     
     
     
